1e89ae98317f7d83e6767a3930200a0b6401de62
Borislav <borislavqkimov@yahoo.com>
2019-03-16 17:51:00 +0200

    Initial

---
+++ app/app/calc.py
@@ -0,0 +1,8 @@
+def add(x,y):
+    """add two number"""
+    return x+y
+
+class CalcTest(TestCase):
+
+    def test_add_numbers(self):
+        """Test"""
---
+++ app/app/test.py
@@ -0,0 +1,2 @@
+from django.test import TestCase
+from app.calc import add
